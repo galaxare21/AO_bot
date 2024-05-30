c87ab510c9f827dc78bf56a2dac5e32860227e72
galaxare21 <98818617+galaxare21@users.noreply.github.com>
2024-05-30 21:21:36 +0200

    Create embed.py
    c'est tout con mais c'est la que sont stocker les embed du bot

---
+++ embed.py
@@ -0,0 +1,18 @@
+
+# embed du menu d'aide
+
+commandss_embed = (
+    "👨‍💻 **/wiki**: Vous allez à la médiatèque chez Iris \n"
+    "\n"
+    "👨‍💻 **/tool**: Vous allez à la salle de brainstorming d'Iris \n"
+    "\n"
+    "👨‍💻 **/trello**: Vous allez espoinner chez Vetex \n"
+    "\n"
+    "👨‍💻**/chart**: Vous trouvez un village minecraft et allez voir le cartographe \n"
+    "\n"
+    "👨‍💻**/forum**: Allez vous plaindre que le jeu est nul sur le forum \n"
+    "\n"
+    "👨‍💻**/value**: Allez voir le cours du marché\n"
+    "\n"
+    "👨‍💻**/lore**: Père castor vous raconte le lore d'arcane odyssey"
+)
